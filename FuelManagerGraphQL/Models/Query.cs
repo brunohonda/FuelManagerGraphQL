8a13c0f60ab4fac2ca5f1e541251c2f19af01932
Bruno Honda <bruno.honda@live.com>
2024-05-05 16:50:01 -0300

    Adicionar arquivos de projeto.

---
+++ FuelManagerGraphQL/Models/Query.cs
@@ -0,0 +1,17 @@
+﻿namespace FuelManagerGraphQL.Models
+{
+    public class Query
+    {
+        [UseProjection]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Veiculo> Veiculos([Service] AppDbContext _context)
+        {
+            return _context.Veiculos;
+        }
+        public IQueryable<Consumo> Consumos([Service] AppDbContext _context)
+        {
+            return _context.Consumos;
+        }
+    }
+}
